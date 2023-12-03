115a74dfce95d0cd0311775a6e3c328fbf135a54
breadworshipper <azhrayashna@gmail.com>
2023-12-03 16:03:30 +0700

    Added SPARQL query to local DB

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+# from SPARQLWrapper import SPARQLWrapper, JSON
+#
+# sparql = SPARQLWrapper("http://localhost:7200/repositories/CarPriceDB")
+# sparql.setQuery("""
+#     SELECT ?s ?o
+#     WHERE {
+#         ?s ?p ?o .
+#     } limit 50
+# """)
+# sparql.setReturnFormat(JSON)
+# results = sparql.query().convert()
+#
+# for result in results["results"]["bindings"]:
+#     print(result["s"]['value'], result["o"]['value'])
 # Create your views here.
