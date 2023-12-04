6fdff45d63c4c20a846b09f434508a8694362621
AndiRaihan <dzaky.raihan@gmail.com>
2023-12-04 13:08:43 +0700

    Added get detail car

--- api/views.py
+++ api/views.py
@@ -74,3 +74,52 @@ def search_query_by_manufacturer(request):
         return JsonResponse(results, safe=False)
 
     return JsonResponse({'message': 'Not Found'}, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+def get_detail_by_id(request, car_id):
+    if request.method == 'GET':
+        sparql = SPARQLWrapper("http://localhost:7200/repositories/CarPriceDB")
+        # Query berdasarkan Manufacturer
+        sparql.setQuery("""
+                    PREFIX : <http://saya-akan-datang.org/data#>
+
+                    SELECT DISTINCT ?CarID ?price ?currency ?Cylinders ?Doors ?engineVolumeAmount ?isEngineTurbo ?engineUnit ?isLeatherInterior ?levyAmount ?levyCurrency ?mileageAmount ?mileageUnit ?prodYear ?airbagsAmount ?color ?driveWheels ?fuelType ?gearBoxType ?carCategory ?carManufacturer ?carModel ?wheel
+                    WHERE {
+                      ?CarID
+                        :Cylinders         ?Cylinders ;
+                        :Doors             ?Doors ;
+                        :Engine_volume     [ :amount   ?engineVolumeAmount ;
+                                             :isTurbo  ?isEngineTurbo ;
+                                             :unit    ?engineUnit
+                                           ] ;
+                        :Leather_interior  ?isLeatherInterior ;
+                        :Mileage           [ :amount  ?mileageAmount ;
+                                             :unit    ?mileageUnit
+                                           ] ;
+                         :Price            [ :amount    ?price ;
+                                             :currency  ?currency
+                                           ] ;
+                        :Prod._year        ?prodYear ;
+                        :airbagsAmount     ?airbagsAmount ;
+                        :hasColor          ?color ;
+                        :hasDriveWheels    ?driveWheels ;
+                        :hasFuelType       ?fuelType ;
+                        :hasGearBoxType    ?gearBoxType ;
+                        :hasMachinetype    [ :hasCarCategory   ?carCategory ;
+                                             :hasManufacturer  ?carManufacturer ;
+                                             :hasModel         ?carModel
+                                           ] ;
+                        :hasWheel          ?wheel .
+                        OPTIONAL {
+                          ?CarID :Levy [ :amount    ?levyAmount ;
+                                         :currency  ?levyCurrency
+                                       ] ;
+                        }
+                      FILTER regex(str(?CarID), "%s", "i")
+                    }
+                    """ % car_id)
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+
+        return JsonResponse(results, safe=False)
+    return JsonResponse({'message': 'Not Found'}, status=status.HTTP_404_NOT_FOUND)
