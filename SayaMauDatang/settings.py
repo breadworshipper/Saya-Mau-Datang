3eecf21e1f433d672aaba2f98a3b7a26f371a417
breadworshipper <azhrayashna@gmail.com>
2023-12-08 13:34:45 +0700

    Requirements.txt update

--- SayaMauDatang/settings.py
+++ SayaMauDatang/settings.py
@@ -38,14 +38,14 @@ SESSION_COOKIE_SAMESITE = 'None'
 
 INSTALLED_APPS = [
     "SayaMauDatang",
-    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "api"
+    "api",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
